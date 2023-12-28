a870d188687c75b9c1fcd032d05fd3547ef71601
Adriano Ferreira <adrianoferrira.dev@gmail.com>
2023-12-28 19:06:22 -0300

    test(login): Add feature test for /login endpoint

---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,96 @@
+<?php
+
+use App\Models\User;
+
+use function Pest\Laravel\postJson;
+
+beforeEach(fn() => $this->url = '/api/login');
+
+test('user can login', function () {
+    $user = User::factory()->create();
+
+    $request = [
+        'email' => $user->email,
+        'password' => 'password',
+        'remember' => false
+    ];
+
+    postJson($this->url, $request)
+        ->assertOk()
+        ->assertJsonStructure(['access_token', 'token_type']);
+});
+
+test('login fails with incorrect credentials', function () {
+    $user = User::factory()->create();
+
+    $request = [
+        'email' => $user->email,
+        'password' => 'wrong-password',
+        'remember' => true
+    ];
+
+    postJson($this->url, $request)
+        ->assertUnprocessable()
+        ->assertJsonValidationErrors(['email', __('auth.failed')]);
+});
+
+test('email should be valid', function () {
+    postJson($this->url, ['email' => 'user@email.com'])
+        ->assertJsonMissingValidationErrors('email', [
+            __('validation.required'),
+            __('validation.string'),
+            __('validation.email')
+        ]);
+});
+
+test('email should be required', function () {
+    postJson($this->url, [])
+        ->assertUnprocessable()
+        ->assertJsonValidationErrors(['email', __('validation.required')]);
+});
+
+test('email should be a string', function () {
+    postJson($this->url, ['email' => 12345])
+        ->assertUnprocessable()
+        ->assertJsonValidationErrors(['email', __('validation.string')]);
+});
+
+test('email should be a valid email address', function (string $email) {
+    postJson($this->url, [compact('email')])
+        ->assertUnprocessable()
+        ->assertJsonValidationErrors(['email', __('validation.email')]);
+})->with(['invalid-email', 'invalid-email@.com']);
+
+test('password should be valid', function () {
+    postJson($this->url, ['password' => 'password'])
+        ->assertJsonMissingValidationErrors('password');
+});
+
+test('password should be required', function () {
+    postJson($this->url, [])
+        ->assertUnprocessable()
+        ->assertJsonValidationErrors(['password', __('validation.required')]);
+});
+
+test('password should be a string', function () {
+    postJson($this->url, ['password' => 12345])
+        ->assertUnprocessable()
+        ->assertJsonValidationErrors(['password', __('validation.string')]);
+});
+
+test('remember should be valid', function () {
+    postJson($this->url, ['remember' => true])
+        ->assertJsonMissingValidationErrors('remember');
+});
+
+test('remember should be required', function () {
+    postJson($this->url, [])
+        ->assertUnprocessable()
+        ->assertJsonValidationErrors(['remember', __('validation.required')]);
+});
+
+test('remember should be a boolean', function () {
+    postJson($this->url, ['remember' => 'not_a_boolean'])
+        ->assertUnprocessable()
+        ->assertJsonValidationErrors(['remember', __('validation.boolean')]);
+});
