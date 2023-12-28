3aed8ef2a2525337864269e6368c8b355ac2dece
Adriano Ferrira <adrianoferrira.dev@gmail.com>
2023-12-27 21:42:48 -0300

    feat: Defines accessors to check user roles
    This commit adds accessors in the User model to facilitate role checks. The accessors include is_main_support, is_manager_support, is_tenant, is_group and is_person.

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Enums\Role;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -31,4 +32,39 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected function isMainSupport(): Attribute
+    {
+        return Attribute::make(
+            get: fn(): bool => $this->role === Role::MAIN_SUPPORT,
+        );
+    }
+
+    protected function isManagerSupport(): Attribute
+    {
+        return Attribute::make(
+            get: fn(): bool => $this->role === Role::MANAGER_SUPPORT,
+        );
+    }
+
+    protected function isTenant(): Attribute
+    {
+        return Attribute::make(
+            get: fn(): bool => $this->role === Role::TENANT,
+        );
+    }
+
+    protected function isGroup(): Attribute
+    {
+        return Attribute::make(
+            get: fn(): bool => $this->role === Role::GROUP,
+        );
+    }
+
+    protected function isPerson(): Attribute
+    {
+        return Attribute::make(
+            get: fn(): bool => $this->role === Role::PERSON,
+        );
+    }
 }
