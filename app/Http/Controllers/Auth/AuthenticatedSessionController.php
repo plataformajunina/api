46d0523c6cd18926b6528388eaafa72bd6d9b880
Adriano Ferreira <adrianoferrira.dev@gmail.com>
2024-01-01 19:23:38 -0300

    refactor(logout): Update destroy function to use Logout action

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Actions\Auth\Login;
+use App\Actions\Auth\{Login, Logout};
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -25,14 +23,10 @@ class AuthenticatedSessionController extends Controller
         ]);
     }
 
-    public function destroy(Request $request): Response
+    public function destroy(): Response
     {
-        Auth::guard('web')->logout();
+        app(Logout::class)->execute();
 
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
-        return response()->noContent();
+        return response(['message' => 'Token Revogado.']);
     }
 }
