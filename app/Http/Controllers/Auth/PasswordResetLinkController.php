9f21d2a980e415c36b6a1eeeb46473338b4d408c
Adriano Ferreira <adrianoferrira.dev@gmail.com>
2024-01-02 20:19:57 -0300

    refactor(forgot-password): Update store function to use PasswordResetLink action
    This commit updates the 'store' function in ForgotPasswordController.

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -2,38 +2,21 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Actions\Auth\PasswordResetLink;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Password;
-use Illuminate\Validation\ValidationException;
+use App\Http\Requests\Auth\PasswordResetLinkRequest;
+use Illuminate\Http\Response;
 
 class PasswordResetLinkController extends Controller
 {
-    /**
-     * Handle an incoming password reset link request.
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function store(Request $request): JsonResponse
+    public function store(PasswordResetLinkRequest $request): Response
     {
-        $request->validate([
-            'email' => ['required', 'email'],
-        ]);
-
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
-            $request->only('email')
+        $status = app(PasswordResetLink::class)->execute(
+            email: $request->email
         );
 
-        if ($status != Password::RESET_LINK_SENT) {
-            throw ValidationException::withMessages([
-                'email' => [__($status)],
-            ]);
-        }
-
-        return response()->json(['status' => __($status)]);
+        return response([
+            'message' => $status
+        ]);
     }
 }
