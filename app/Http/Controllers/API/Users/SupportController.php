c6277daeaf83d2d526055e0133848aa6e29eb05b
Adriano Ferreira <adrianoferrira.dev@gmail.com>
2024-01-07 20:06:03 -0300

    feat(supports): Add SupportController with store method implementation

---
+++ app/Http/Controllers/API/Users/SupportController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\API\Users;
+
+use App\Actions\Users\Support\CreateSupport;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Users\Support\StoreSupportRequest;
+use App\Http\Resources\Users\SupportResource;
+use App\Models\Support;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class SupportController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(StoreSupportRequest $request): Response
+    {
+        $supportCreated = app(CreateSupport::class)->execute(
+            role: $request->role,
+            name: $request->name,
+            email: $request->email
+        );
+
+        return response(
+            content: new SupportResource($supportCreated),
+            status: Response::HTTP_CREATED
+        );
+    }
+
+    public function show(Support $support)
+    {
+        //
+    }
+
+    public function update(Request $request, Support $support)
+    {
+        //
+    }
+
+    public function destroy(Support $support)
+    {
+        //
+    }
+}
