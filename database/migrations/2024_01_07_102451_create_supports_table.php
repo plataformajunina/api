974b15ac82f4d2d0b7a0f61973e23f158c600fdc
Adriano Ferreira <adrianoferrira.dev@gmail.com>
2024-01-07 13:59:59 -0300

    feat(migrations): Add supports table migration

---
+++ database/migrations/2024_01_07_102451_create_supports_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Enums\SupportRole;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('supports', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignIdFor(User::class);
+            $table->string('role', 20)->default(SupportRole::MANAGER->value);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('supports');
+    }
+};
