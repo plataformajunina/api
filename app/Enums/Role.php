55e6c41d72415dc2c8908790328dc47fdb4ace0d
Adriano Ferreira <adrianoferrira.dev@gmail.com>
2024-01-07 13:50:27 -0300

    refactor(acl): Update Role enum
    This commit removes cases MAIN_SUPPORT and MANAGER_SUPPORT, adds SUPPORT case.

--- app/Enums/Role.php
+++ app/Enums/Role.php
@@ -4,8 +4,7 @@ namespace App\Enums;
 
 enum Role: string
 {
-    case MAIN_SUPPORT = 'main_support';
-    case MANAGER_SUPPORT = 'manager_support';
+    case SUPPORT = 'support';
     case TENANT = 'tenant';
     case GROUP = 'group';
     case PERSON = 'person';
