6423917e641e743380ec5e7dda9ca946a8d04d37
Adriano Ferreira <adrianoferrira.dev@gmail.com>
2024-01-07 19:49:49 -0300

    feat(migrations): Add softDeletes to supports table migration

--- database/migrations/2024_01_07_102451_create_supports_table.php
+++ database/migrations/2024_01_07_102451_create_supports_table.php
@@ -8,22 +8,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('supports', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->foreignIdFor(User::class);
             $table->string('role', 20)->default(SupportRole::MANAGER->value);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('supports');
